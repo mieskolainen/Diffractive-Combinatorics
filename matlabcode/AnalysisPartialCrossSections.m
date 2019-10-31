cb70269e123c4626761859e35a2f62f60ab8d5d2
Mikael Mieskolainen <mikael.mieskolainen@cern.ch>
2019-10-31 20:40:14 +0200

    clean up

--- matlabcode/AnalysisPartialCrossSections.m
+++ matlabcode/AnalysisPartialCrossSections.m
@@ -1,38 +1,37 @@
 % Plot Partial Combinatorial Cross Sections
 %
-% mikael.mieskolainen@cern.ch, 2018
+% mikael.mieskolainen@cern.ch, 2019
 % Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 
 clear; close all;
 addpath ./src
 param.basepath = '/home/user/cernbox/ALICE/offline_new/figures_xsec';
 
-%param.comb_level = 1;   % Detector level
-param.comb_level = 2;    % Fiducial (unfolded particle) level
-
 % Unfolding iteration range
-param.hih = 8;           % Maximum iteration
-param.cen = 2;           % Central value
-param.low = 1;           % Minimum iteration
+param.hih = 6;           % Maximum iteration
+param.cen = 4;           % Central value
+param.low = 2;           % Minimum iteration
 
 % *** SET HERE MANUALLY BASED ON C++ CODE + EXTERNAL INFO ***
 param.relative_lumi_uncertainty = 1.2 / 57.8; % V0-AND vdM +- / V0-AND vdM value
 param.sigma_inel_fid = 72;                    % Measured total fiducial in data
-param.sigma_inel_MC  = 79;                    % Total inelastic in MC
+param.sigma_inel_MC  = 80;                    % Total inelastic in MC
 
-%param.run = 274593;
-%param.run = 274594;
-param.run = 274595;
+% First one is the central, others are used for run-by-run variations
+param.runs = [274595, 274593, 274594];
 
 PLOT_ON = false;
 DATA_ON = true;
+FIT_ON  = true;
 for model = 1
     param.model = model;
-    [mins, acceptances, METRICS] = Combinatorial(param, PLOT_ON, DATA_ON);
+    [mins, acceptances, METRICS] = Combinatorial(param, PLOT_ON, DATA_ON, FIT_ON);
 end
 
 
 %% Print out inelastic box figure
+
+if (FIT_ON)
 
 fig = figure;
 
@@ -51,12 +50,12 @@ rectangle('Position', [mu_fid-uncert_fid 0.67 uncert_fid 0.16], ...
 
 errorbar(mu_fid, 0.75, 0, 0, 0.5, 0.5,'ks', 'MarkerSize', 11); hold on;
 
-text(mu_fid*0.978,  0.88, '$\sigma_{\mathrm{fid}}^{measured}$','interpreter','latex','fontsize',22);
+text(mu_fid*0.993,  0.88, '$\sigma_{\mathrm{fid}}^{measured}$','interpreter','latex','fontsize',22);
 text(mu_fid*0.978,  0.60, 'Fiducial (approx):','interpreter','latex','fontsize', 12);
 text(mu_fid*0.978,  0.55, '$-7.0 < \eta_{ch} < 6.3$','interpreter','latex','fontsize', 12);
 text(mu_fid*0.978,  0.51, '$p_T > 50$ MeV','interpreter','latex','fontsize', 12);
 
-text(mu_fid*0.982,  0.80, 'Luminosity','interpreter','latex','fontsize',10);
+text(mu_fid*0.987,  0.69, 'Luminosity','interpreter','latex','fontsize',10);
 plot(mu_fid,        0.75, 'ks', 'MarkerSize', 12); hold on;
 
 color = ones(3,1)*0.9;
@@ -97,3 +96,5 @@ box on;
 outputfile = 'inelastic.pdf';
 print(fig, sprintf('./combfigs/%s', outputfile), '-dpdf');
 system(sprintf('pdfcrop --margins 10 ./combfigs/%s ./combfigs/%s', outputfile, outputfile));
+
+end
